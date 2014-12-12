ce2f03ddd26d290ee27c08e99adba0c4033d95f9
Luciano Arango <lucianoarango@college.harvard.edu>
2014-12-11 22:24:41 -0500

    packaged extension

--- client/H3T/background.js
+++ client/H3T/background.js
@@ -1,88 +1,131 @@
+/*
+Background.js
+Code that examining headers to find redirects to h3t service, handling
+them correctly. Also takes care of storing tokens and logs of website visits,
+sending them to h3t service provider when needed.
+*/
+
+
+
 // Parses URLs
 var parser = document.createElement('a');
+
+// Stores current Redirect being delt with
 var Redirect = {request_id:0, requesting_url: ""}
-// Keep information about redirects to HT-Service Prodiver
+
+// Check if the redirect is headed to h3t provider
 chrome.webRequest.onBeforeRedirect.addListener(
 function(details) {
-	// If its redirecting to HT-Provider
-	if(details.redirectUrl == "https://www.google.com/?gws_rd=ssl"){
-		request = parseInt(details.requestId)
-		// If we are already tracking this redirect, do nothin
-		if(Redirect.request_id == request) return
-		else {
-			Redirect.request_id = request
-			parser.href = details.url
-			Redirect.requesting_url = parser.hostname
+  // If its redirecting to HT-Provider
+  if(details.redirectUrl == "http://h3t.herokuapp.com/redirect"){
 
+    // Lets keep this request in mind
+    request = parseInt(details.requestId)
+    // If we are already tracking this redirect, do nothin
+    if(Redirect.request_id == request) return
+    // Store the domain of redirecting website
+    Redirect.request_id = request
+    parser.href = details.url
+    Redirect.requesting_url = parser.hostname.replace("www.","")
 
-		}
-	}
+  }
 }, {urls: ["<all_urls>"]});
 
-// Modify Request going to HT-Provider
+/*
+Modify Request going to HT-Provider so that they contain information about
+requesting website
+*/
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details) { console.log("onBeforeRequest");
-  // We've been redirected to HT-Provider
+  function(details) {
+
   request = parseInt(details.requestId)
+  // We've been redirected to HT-Provider
   if(Redirect.request_id == request){
-  	// We should be going to HT-Provider
-  	 if(details.url.indexOf("google.com/?gws_rd=ssl") > -1){
-  	 	console.log(Redirect)
-  		url = 'http://h3t.herokuapp.com/token?domain=' + Redirect.requesting_url.replace("www.","")
-  		return {redirectUrl:url }
-  	 }
+    // We should be going to HT-Provider
+     if(details.url.indexOf("h3t.herokuapp.com/redirect") > -1){
+      parser.href = details.url
+      // Add information about requesting site
+      url = 'http://h3t.herokuapp.com/token?domain=' + Redirect.requesting_url
+      return {redirectUrl:url }
+     }
   }
   },
   {urls: ["<all_urls>"]},
   ["blocking"]);
+/*
+Everytime we send a request, service keeps track of times the site
+has been visited
+*/
+chrome.webRequest.onSendHeaders.addListener(
+function(headers){
+  // Parse url to domain
+  parser.href = headers.url
+  var host = parser.hostname.replace("www.","")
+  // Get the amount we currently have and increment it
+   chrome.storage.local.get(host, function(item){
+    var counter= {}
+    // We're already keeping track of this service
+    if (host in item ){
+      counter[host] = item[host]  + 1
+    }
+    else {
+      counter[host]  = 1
+    }
+    // Set new amount
+    chrome.storage.local.set(counter)
+    chrome.storage.local.get(host, function(item){
+    console.log(item)
+    })
+  })
 
-// chrome.webRequest.onBeforeSendHeaders.addListener(
-//   function(details) {
-//     for (var i = 0; i < details.requestHeaders.length; ++i) {
-//       if (details.requestHeaders[i].name === 'User-Agent') {
-//         // details.requestHeaders[i].value = "HT"
-//         // details.requestHeaders.splice(i, 1);
-//         break;
-//       }
-//     }
-//     if(details.url.indexOf("www.h3t.com") > -1){
-//     	console.log("landed")
-//     	details.url = 'http://www.reddit.com'
-//     	//chrome.tabs.create({url:'http://www.google.com'}, function(tab){})
-//     	console.log("foo")
-//     }
+},{urls: ["<all_urls>"]});
+/*
+If cookies have been changed for this service then lets send an update
+to the H3T Service Provider and reset the counter
+*/
+chrome.cookies.onChanged.addListener(function (obj_cookie){
+  // Is it an HT-Token cookie?
+  if(obj_cookie.cookie.name == "HT-Token"){
+    var url = ""
+    // Deal with (some) urls that start with .
+    if(obj_cookie.cookie.domain[0] == "." ){
+      url =  obj_cookie.cookie.domain.substring(1, obj_cookie.cookie.domain.length);
+    }
+    else{
+      url = obj_cookie.cookie.domain
+    }
+    // Lets get the counter for that cookie
+    chrome.storage.local.get(url, function(item){
+     // Return if there is no information for that website
+      if(!item) return
+      var req = new XMLHttpRequest();
+      // Sends message with counter H3T Service provider via POST
+      req.open("POST","http://h3t.herokuapp.com/log",true);
+      var params = "domain="+ url + "&count="+item[url].toString();
+      //Send the proper header information along with the request
+      req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      req.send(params)
 
-//     return { requestHeaders: details.requestHeaders};
-//   },
-//   {urls: ["<all_urls>"]},
-//   ["blocking", "requestHeaders"]);
-// chrome.webRequest.onResponseStarted.addListener(
-// 	function(response){
-// 		//console.log(response)
-// 	}, {urls: ["<all_urls>"]});
 
+
+
+    })
+     // Reset Cookie
+    var updated_counter = {}
+    updated_counter[url] = 0
+    chrome.storage.local.set(updated_counter)
+  }
+});
+
+// Set cookies that we receive from the token website
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	if (sender.tab){
-  		console.log("Received message from "+sender.tab.url)
-  	}
+    if (sender.tab){
+      console.log("Received message from "+sender.tab.url)
+    }
   if(request.cookie){
-		chrome.cookies.set(request.cookie)
-		var worked = false
-		sendResponse({cookie_status: "success"})
-	}
-		// chrome.cookies.get({url:request.cookie.url,name:request.cookie.name}, function(check_cookie){
-
-		// 	if(check_cookie.name == request.cookie.name )	{
-		// 		worked = true
-		// 		sendResponse({cookie_status: "success"})
-		// 	}
-		// 	else {
-		// 		sendResponse({cookie_status: "success"})
-		// 	}
-		// })
-		// if (worked) sendResponse({cookie_status: "success"})
-		// }
-
-
-  });
+    chrome.cookies.set(request.cookie)
+    var worked = false
+    sendResponse({cookie_status: "success"})
+  }
+});
--- client/H3T/hashes.js
+++ client/H3T/hashes.js
@@ -1,21 +1,33 @@
+/*
+Hashes.js
+Code that handles body of response from H3T Server. Collecting
+the token, sending it to be stored as well as redirecting the user
 
+*/
 
 // Are we at the site for tokens?
-if(window.location.href.indexOf("/token")){
+if(window.location.href.indexOf("/token") > -1){
 	if (document.body.innerText == "{}"){
 		no_token()
 	}
 	else {
-		var token = JSON.parse(document.body.innerText)
+		var token
+		// Try to force into an object
+		try {
+			 token = JSON.parse(document.body.innerText)
+		}
+		catch(err) {
+    	console.log(err)
+		}
+
 		// Check again that we have a token
 		if(Object.keys(token).length == 0) no_token()
+		// We have a token that has a domain and a mesasge
+		if(token.domain && token.message){
+			// Send message to background to set coookies
+			chrome.runtime.sendMessage({cookie:{url:"http://www."+token.domain, name:'HT-Token', value: token.message ,expirationDate: token.expiration, domain:token.domain, httpOnly:false }}, function(response) {
 
-		if(token.domain && token.payload){
-			// Expiration
-			var milliseconds = (new Date).getTime() + 100;
-
-			chrome.runtime.sendMessage({cookie:{url:"http://www."+token.domain, name:'HT-Token', value: token.payload ,expirationDate: milliseconds, domain:token.domain, httpOnly:false }}, function(response) {
- 					 console.log(response)
+					// If everything worked redirect
  					 if (response.cookie_status == "success"){
  					 	window.location = 'http://'+token.domain
  					 }
@@ -24,13 +36,13 @@ if(window.location.href.indexOf("/token")){
 	}
 
 }
-
-
+// Deal with server not returning a Token
 function no_token(){
 	//Inform user
 	document.body.innerHTML +="<h1>You are either not logged in or do not have access to this site</h1>"
 	// Redirect user to main page
-	window.setTimeout(function(){window.location = window.location.href.host;}, 3000);
+	window.setTimeout(function(){
+		window.location = window.location.href.host ;}, 2000);
 
 }
 
